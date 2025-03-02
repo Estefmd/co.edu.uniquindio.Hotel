c34ddae95b0d74eefc5e190f86a691217c7c8e66
Estefania Montoya Davila <142418723+Estefmd@users.noreply.github.com>
2025-03-02 11:41:37 -0500

    Se ejecuta Main con modelFactory
    Se prueba el Proyecto.

--- co.edu.uniquindio.Hotel/src/main/java/co/edu/uniquindio/Hotel/Factory/ModelFactory.java
+++ co.edu.uniquindio.Hotel/src/main/java/co/edu/uniquindio/Hotel/Factory/ModelFactory.java
@@ -2,43 +2,36 @@ package co.edu.uniquindio.Hotel.Factory;
 
 import co.edu.uniquindio.Hotel.Modelo.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class ModelFactory {
     public static ModelFactory instance;
     private Hotel hotel;
 
     private ModelFactory(){
-        hotel = inicializarDatos();
+        hotel = new Hotel(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        inicializarDatos();
     }
-
     public static ModelFactory getInstance() {
         if (instance == null) {
             instance = new ModelFactory();
         }
         return instance;
     }
+    private void inicializarDatos() {
+        Habitacion habitacion1 = new Habitacion(101, "Doble", 200000);
+        Habitacion habitacion2 = new Habitacion(102, "Suite", 350000);
+        hotel.agregarHabitacion(habitacion1);
+        hotel.agregarHabitacion(habitacion2);
 
-    private Hotel inicializarDatos() {
-        Hotel hotel = new Hotel();
-        Cliente cliente1 = new Cliente();
-        cliente1.setNombre("Juan");
-        cliente1.setNumeroCedula("1094907106");
+        Cliente cliente1 = hotel.crearCliente("Juan Pérez", "12345678");
+        Cliente cliente2 = hotel.crearCliente("María López", "87654321");
 
-        Habitacion habitacion1 = new Habitacion();
-        habitacion1.setNumeroHabitacion(101);
-        habitacion1.setTipoHabitacion("Suite");
-        habitacion1.setPrecioHabitacion(300.000);
-
-        Reserva reserva1 = new Reserva();
-        reserva1.setCliente(cliente1);
-        reserva1.setHabitacion(habitacion1);
-
-        hotel.getListaClientes().add(cliente1);
-        hotel.getListaHabitaciones().add(habitacion1);
-        hotel.getListaReservas().add(reserva1);
-
-        return hotel;
+        LocalDate fechaIngreso = LocalDate.of(2025, 3, 10);
+        LocalDate fechaSalida = LocalDate.of(2025, 3, 15);
+        Reserva reserva1 = hotel.agregarReserva(habitacion1, cliente1, fechaIngreso, fechaSalida);
     }
-
     public Hotel getHotel() {
 
         return hotel;
